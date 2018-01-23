173184ff3450b767ad027c9a558048efcc7aa5f3
Noe <noe.joaquin.james@gmail.com>
2018-01-23 16:26:24 -0500

    make sure tables slice of state is an array

--- frontend/reducers/tables_reducer.js
+++ frontend/reducers/tables_reducer.js
@@ -4,7 +4,7 @@ import {RECEIVE_TABLES} from '../actions/table_actions';
 const TablesReducer = (state = {}, action) => {
   switch(action.type){
     case RECEIVE_TABLES:
-      return action.tables;
+      return JSON.parse(action.tables);
     default:
       return state;
   }
