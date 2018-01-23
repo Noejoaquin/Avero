055b81a8aceb38a1934d3b1bead41041f1f5f260
Noe <noe.joaquin.james@gmail.com>
2018-01-23 14:28:02 -0500

    modify webpack.config, create app.jsx, root.jsx

---
+++ frontend/components/app.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+const App = () => (
+  <div className = 'app'>
+    <h1>AVERO</h1>
+  </div>
+);
+
+export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 var path = require('path');
 
 module.exports = {
-  entry: './frontend/app.jsx',
+  entry: './frontend/avero.jsx',
   output: {
     filename: './bundle.js',
   },
