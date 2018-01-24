5919bd9d41da0813ca76bddcef644be2706649ab
Noe <noe.joaquin.james@gmail.com>
2018-01-24 15:28:00 -0500

    create error actions

--- frontend/actions/check_actions.js
+++ frontend/actions/check_actions.js
@@ -2,7 +2,13 @@ import * as CheckApiUtil from '../util/check_util';
 
 export const RECEIVE_CHECKS = "RECEIVE_CHECKS";
 export const RECEIVE_CHECK = 'RECEIVE_CHECK';
-export const REMOVE_CHECKS = 'REMOVE_CHECKS'
+export const REMOVE_CHECKS = 'REMOVE_CHECKS';
+export const RECEIVE_CHECK_ERRORS = "RECEIVE_CHECK_ERRORS";
+
+export const receiveErrors = (errors) => ({
+  type: RECEIVE_CHECK_ERRORS,
+  errors
+})
 
 export const receiveChecks = (checks) => ({
   type: RECEIVE_CHECKS,
@@ -21,6 +27,7 @@ export const removeChecks = () => ({
 export const fetchChecks = () => dispatch => {
   return CheckApiUtil.fetchChecks()
                      .then((checks) => dispatch(receiveChecks(checks)),
+                      (errors) => dispatch(receiveErrors(errors))
                     );
 };
 
@@ -28,6 +35,7 @@ export const fetchChecks = () => dispatch => {
 export const fetchCheck = (checkId) => dispatch => {
   return CheckApiUtil.fetchCheck(checkId)
                      .then((check) => dispatch(receiveCheck(check)),
+                      (errors) => dispatch(receiveErrors(errors))
                     );
 };
 
@@ -35,29 +43,34 @@ export const fetchCheck = (checkId) => dispatch => {
 export const createCheck = (tableId) => dispatch => {
   return CheckApiUtil.createCheck(tableId)
                      .then((check) => dispatch(receiveCheck(check)),
+                      (errors) => dispatch(receiveErrors(errors))
                     );
 };
 
 export const addItemOnCheck = (id, itemId) => dispatch => {
   return CheckApiUtil.addItemOnCheck(id, itemId)
                      .then((check) => dispatch(receiveCheck(check)),
+                      (errors) => dispatch(receiveErrors(errors))
                     );
 };
 
 export const voidItemOnCheck = (id, orderedItemId) => dispatch => {
   return CheckApiUtil.voidItemOnCheck(id, orderedItemId)
                      .then((check) => dispatch(receiveCheck(check)),
+                      (errors) => dispatch(receiveErrors(errors))
                     );
 };
 
 export const closeCheck = (checkId) => dispatch => {
   return CheckApiUtil.closeCheck(checkId)
                      .then((check) => dispatch(receiveCheck(check)),
+                      (errors) => dispatch(receiveErrors(errors))
                     );
 };
 
 export const deleteChecks = () => dispatch => {
   return CheckApiUtil.deleteChecks()
                      .then((checks) => dispatch(removeChecks(checks)),
+                      (errors) => dispatch(receiveErrors(errors))
                     );
 };
--- frontend/actions/table_actions.js
+++ frontend/actions/table_actions.js
@@ -1,7 +1,12 @@
 import * as TableApiUtil from '../util/table_util';
 
 export const RECEIVE_TABLES = "RECEIVE_TABLES";
+export const RECEIVE_CHECK_ERRORS = "RECEIVE_CHECK_ERRORS";
 
+export const receiveErrors = (errors) => ({
+  type: RECEIVE_CHECK_ERRORS,
+  errors
+})
 
 export const receiveTables = (tables) => ({
   type: RECEIVE_TABLES,
@@ -11,5 +16,6 @@ export const receiveTables = (tables) => ({
 export const fetchTables = () => dispatch => {
   return TableApiUtil.fetchTables()
                      .then((tables) => dispatch(receiveTables(tables)),
+                      (errors) => dispatch(receiveErrors(errors))
                     );
 };
