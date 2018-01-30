74242c97eb6143da5e2a827f40d013a998608fb3
Noe <noe.joaquin.james@gmail.com>
2018-01-29 19:35:34 -0500

    make alternate show

--- frontend/components/checks/check_show.jsx
+++ frontend/components/checks/check_show.jsx
@@ -7,6 +7,7 @@ class CheckShow extends React.Component {
     this.check = this.props.check;
     this.fetchCheck = this.props.fetchCheck;
     this.tableId = this.props.tableId;
+    this.handleAddItem = this.handleAddItem.bind(this);
   }
 
   componentDidMount(){
@@ -19,9 +20,31 @@ class CheckShow extends React.Component {
     this.closeCheck(id)
   }
 
+  handleAddItem() {
+
+  }
+
   render(){
     if (this.props.check === undefined){
       return null
+    } else if (this.props.check.closed === true ){
+      debugger
+      return (
+        <div id={status} key={this.props.check.id} className='check-index-item-show'>
+          <li>
+            {this.props.check.id}
+          </li>
+          <li>
+            {this.props.check.tableId}
+          </li>
+          <li>
+            {this.props.check.closed === false ? 'OPEN' : 'CLOSED'}
+          </li>
+            {this.props.check.orderedItems && this.props.check.orderedItems.length === 0 ? 'NO ITEMS ON THIS CHECK' : this.props.check.orderedItems}
+          <li>
+          </li>
+        </div>
+      )
     } else {
       return (
         <div id={status} key={this.props.check.id} className='check-index-item-show'>
@@ -36,6 +59,7 @@ class CheckShow extends React.Component {
           </li>
             {this.props.check.orderedItems && this.props.check.orderedItems.length === 0 ? 'NO ITEMS ON THIS CHECK' : this.props.check.orderedItems}
           <li>
+            <button onClick={this.handleAddItem}>Add Item</button>
           </li>
         </div>
       )
