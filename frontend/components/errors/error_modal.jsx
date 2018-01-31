affa976f9ef696e38655ca47e7dbdf5be58f720e
Noe <noe.joaquin.james@gmail.com>
2018-01-31 12:27:09 -0500

    style error modal

--- frontend/components/errors/error_modal.jsx
+++ frontend/components/errors/error_modal.jsx
@@ -5,8 +5,10 @@ import { Link } from 'react-router-dom';
 export const ErrorModal = ({description}) => {
   return (
       <div className='error-modal modal'>
+        <h1 className='modal-list modal-header error-modal-header'>Error!</h1>
         <ul className='error-list modal-list'>
-          <li>{description}</li>
+          <li className='error-list-item'>{description}</li>
+          <li className='error-list-item'>Please Close the Currently Open Check If You Wish To Open A New One</li>
         </ul>
       </div>
   )
