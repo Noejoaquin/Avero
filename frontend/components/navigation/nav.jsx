691a28c40ebb98037d5426b35eed5d08ad165f6a
Noe <noe.joaquin.james@gmail.com>
2018-01-30 19:16:12 -0500

    basic styling for nav

--- frontend/components/navigation/nav.jsx
+++ frontend/components/navigation/nav.jsx
@@ -3,10 +3,10 @@ import { Link } from 'react-router-dom';
 
 
 export const NavBar = ({ path }) => {
-  let linkToTables = path === '/' ? <li></li> : <Link to='/'><button>Back To Tables</button></Link>
+  let linkToTables = path === '/' ? <li></li> : <button className='nav-button'><Link className='nav-link' to='/' >Back To Tables</Link></button>
   return (
-        <div>
-          <h1>LARAVAL</h1>
+        <div className='nav-container'>
+          <h1 className='nav-header'>LARAVAL</h1>
           {linkToTables}
         </div>
   )
