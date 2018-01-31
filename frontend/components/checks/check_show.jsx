739c251a51a1df82b3e786bc52b133ec8b2a3ed8
Noe <noe.joaquin.james@gmail.com>
2018-01-31 16:49:02 -0500

    fix syntax

--- frontend/components/checks/check_show.jsx
+++ frontend/components/checks/check_show.jsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import { Route } from 'react-router-dom';
-import ItemIndexContainer from '../items/items_index_container';
+import React from "react";
+import { Route } from "react-router-dom";
+import ItemIndexContainer from "../items/items_index_container";
 
 class CheckShow extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.check = this.props.check;
     this.fetchCheck = this.props.fetchCheck;
@@ -17,168 +17,205 @@ class CheckShow extends React.Component {
     this.order = this.order.bind(this);
   }
 
-  componentDidMount(){
-    this.fetchItems().then(() => this.fetchCheck(this.props.checkId))
+  componentDidMount() {
+    this.fetchItems().then(() => this.fetchCheck(this.props.checkId));
   }
 
-  componentWillReceiveProps(newProps){
-    if (this.props.checkId !== newProps.checkId){
-      this.fetchCheck(newProps.checkId)
+  componentWillReceiveProps(newProps) {
+    if (this.props.checkId !== newProps.checkId) {
+      this.fetchCheck(newProps.checkId);
     }
   }
 
-  handleCloseCheck(id){
-    this.closeCheck(id)
+  handleCloseCheck(id) {
+    this.closeCheck(id);
   }
 
-  handleVoidItem(checkId, orderedItemId){
-    this.voidItemOnCheck(checkId, orderedItemId).then(() => this.fetchCheck(this.props.checkId))
+  handleVoidItem(checkId, orderedItemId) {
+    this.voidItemOnCheck(checkId, orderedItemId).then(() =>
+      this.fetchCheck(this.props.checkId)
+    );
   }
 
-  toggleModal(){
-    let modal = document.getElementsByClassName('item-modal')[0]
-    let button = document.getElementsByClassName('item-modal-button')[0]
-    if (modal.classList.contains('showing')){
-      modal.style.display = 'none'
+  toggleModal() {
+    let modal = document.getElementsByClassName("item-modal")[0];
+    let button = document.getElementsByClassName("item-modal-button")[0];
+    if (modal.classList.contains("showing")) {
+      modal.style.display = "none";
     } else {
-      window.addEventListener('click', function(e) {
+      window.addEventListener("click", function(e) {
         if (e.target == modal || e.target == button) {
-          modal.style.display = 'none'
+          modal.style.display = "none";
         }
-      })
-      modal.style.display = 'block'
+      });
+      modal.style.display = "block";
     }
   }
 
-  order(items){
+  order(items) {
     // debugger
-    if (items === undefined) { return null }
+    if (items === undefined) {
+      return null;
+    }
     // debugger
-    let times = items.map( item => item.dateCreated )
-    let timesSorted = times.sort().reverse()
+    let times = items.map(item => item.dateCreated);
+    let timesSorted = times.sort().reverse();
     let sortedItems = [];
-    for (let i = 0; i < timesSorted.length; i++ ){
-      for (let j = 0; j < timesSorted.length; j ++) {
-        if (times[i] === items[j].dateCreated){
-          sortedItems.push(items[j])
+    for (let i = 0; i < timesSorted.length; i++) {
+      for (let j = 0; j < timesSorted.length; j++) {
+        if (times[i] === items[j].dateCreated) {
+          sortedItems.push(items[j]);
         }
       }
     }
     // debugger
-    return sortedItems
+    return sortedItems;
   }
 
-  createCheckItemList(items, status){
+  createCheckItemList(items, status) {
     let orderedItems;
     let unvoidedItems;
     let voidItems;
     let that = this;
-    if (items === undefined){
-      return undefined
+    if (items === undefined) {
+      return undefined;
     } else {
       orderedItems = this.order(items);
       unvoidedItems = [];
-      voidItems = []
+      voidItems = [];
       // debugger
-      for (let i = 0; i < items.length; i++){
-        if (orderedItems[i].voided === true && status === 'closed') { continue }
-        for (let j = 0; j < this.props.items.length; j++){
-          if (orderedItems[i].itemId === this.props.items[j].id){
-            if (orderedItems[i].voided === true) {
-              debugger
-              voidItems.push(
-              <ul key={orderedItems[i].id} className='item-information'>
-                <li>{this.props.items[j].name}</li>
-                <li>${this.props.items[j].price}</li>
-                {orderedItems[i].voided === true && status === 'open' ? <li>Voided</li> : <li id='empty'></li>}
-              </ul>
-            )
+      for (let i = 0; i < items.length; i++) {
+        if (orderedItems[i].voided === true && status === "closed") {
           continue;
         }
+        for (let j = 0; j < this.props.items.length; j++) {
+          if (orderedItems[i].itemId === this.props.items[j].id) {
+            if (orderedItems[i].voided === true) {
+              debugger;
+              voidItems.push(
+                <ul key={orderedItems[i].id} className="item-information">
+                  <li>{this.props.items[j].name}</li>
+                  <li>${this.props.items[j].price}</li>
+                  {orderedItems[i].voided === true && status === "open" ? (
+                    <li>Voided</li>
+                  ) : (
+                    <li id="empty" />
+                  )}
+                </ul>
+              );
+              continue;
+            }
             unvoidedItems.push(
-              <ul key={orderedItems[i].id} className='item-information'>
+              <ul key={orderedItems[i].id} className="item-information">
                 <li>{this.props.items[j].name}</li>
                 <li>${this.props.items[j].price}</li>
-                {orderedItems[i].voided === false && status === 'open' ? <li><button className='void-item-button' onClick={() => this.handleVoidItem(this.props.checkId, items[i].id)}>Void This Item</button></li> : <li id='empty'></li>}
+                {orderedItems[i].voided === false && status === "open" ? (
+                  <li>
+                    <button
+                      className="void-item-button"
+                      onClick={() =>
+                        this.handleVoidItem(this.props.checkId, items[i].id)
+                      }
+                    >
+                      Void This Item
+                    </button>
+                  </li>
+                ) : (
+                  <li id="empty" />
+                )}
               </ul>
-            )
+            );
           }
         }
       }
     }
-    unvoidedItems.push(voidItems)
-    return unvoidedItems
+    unvoidedItems.push(voidItems);
+    return unvoidedItems;
   }
 
-  render(){
-    if (this.props.check === undefined){
-      return null
+  render() {
+    if (this.props.check === undefined) {
+      return null;
     } else {
-        if (this.props.check.closed === true ){
-          let orderedItems = this.createCheckItemList(this.props.check.orderedItems, 'closed')
-          return (
-            <div id={status} key={this.props.check.id} className='check-index-item-show'>
-              <ul className='basic-check-info-list'>
-                <li className='check-id'>
-                  Check ID: {this.props.check.id}
-                </li>
-                <li className='check-table-id'>
-                  Check Table ID: {this.props.check.tableId}
-                </li>
-                <li className='check-status'>
-                  Check Status: {this.props.check.closed === false ? 'OPEN' : 'CLOSED'}
-                </li>
-              </ul>
-              <ul className='ordered-items-list closed-check-items'>
-                {orderedItems}
-              </ul>
-              <ul className='closed-check-details'>
-                <li>
-                  Tip: {this.props.check.tip}
-                </li>
-                <li>
-                  Tax: {this.props.check.tax}
-                </li>
-              </ul>
-            </div>
-          )
-        } else {
-          let orderedItems = this.createCheckItemList(this.props.check.orderedItems, 'open')
-          return (
-            <div id={status} key={this.props.check.id} className='check-index-item-show'>
-              <ul className='basic-check-info-list'>
-                <li className='check-id'>
-                  Check ID: {this.props.check.id}
-                </li>
-                <li className='check-table-id'>
-                  Check Table ID: {this.props.check.tableId}
-                </li>
-                <li className='check-status'>
-                  Check Status: {this.props.check.closed === false ? 'OPEN' : 'CLOSED'}
-                </li>
-                <button className='close-check-button-show' onClick={() => this.handleCloseCheck(this.props.checkId)}>
-                  Close Check
-                </button>
-              </ul>
-              <div className='item-container'>
-                <button className='add-item-button' onClick={this.toggleModal}>Add Item</button>
-                <div className='item-modal'>
-                  <div className='modal-list exit-button-container exit-button-container-items'>
-                    <button className='modal-list exit-modal-button item-modal-button'>X</button>
-                  </div>
-                  <h1 className='modal-list modal-header modal-list-header'>Menu Items</h1>
-                  <ItemIndexContainer checkId={this.props.check.id}/>
+      if (this.props.check.closed === true) {
+        let orderedItems = this.createCheckItemList(
+          this.props.check.orderedItems,
+          "closed"
+        );
+        return (
+          <div
+            id={status}
+            key={this.props.check.id}
+            className="check-index-item-show"
+          >
+            <ul className="basic-check-info-list">
+              <li className="check-id">Check ID: {this.props.check.id}</li>
+              <li className="check-table-id">
+                Check Table ID: {this.props.check.tableId}
+              </li>
+              <li className="check-status">
+                Check Status:{" "}
+                {this.props.check.closed === false ? "OPEN" : "CLOSED"}
+              </li>
+            </ul>
+            <ul className="ordered-items-list closed-check-items">
+              {orderedItems}
+            </ul>
+            <ul className="closed-check-details">
+              <li>Tip: {this.props.check.tip}</li>
+              <li>Tax: {this.props.check.tax}</li>
+            </ul>
+          </div>
+        );
+      } else {
+        let orderedItems = this.createCheckItemList(
+          this.props.check.orderedItems,
+          "open"
+        );
+        return (
+          <div
+            id={status}
+            key={this.props.check.id}
+            className="check-index-item-show"
+          >
+            <ul className="basic-check-info-list">
+              <li className="check-id">Check ID: {this.props.check.id}</li>
+              <li className="check-table-id">
+                Check Table ID: {this.props.check.tableId}
+              </li>
+              <li className="check-status">
+                Check Status:{" "}
+                {this.props.check.closed === false ? "OPEN" : "CLOSED"}
+              </li>
+              <button
+                className="close-check-button-show"
+                onClick={() => this.handleCloseCheck(this.props.checkId)}
+              >
+                Close Check
+              </button>
+            </ul>
+            <div className="item-container">
+              <button className="add-item-button" onClick={this.toggleModal}>
+                Add Item
+              </button>
+              <div className="item-modal">
+                <div className="modal-list exit-button-container exit-button-container-items">
+                  <button className="modal-list exit-modal-button item-modal-button">
+                    X
+                  </button>
                 </div>
+                <h1 className="modal-list modal-header modal-list-header">
+                  Menu Items
+                </h1>
+                <ItemIndexContainer checkId={this.props.check.id} />
               </div>
-              <ul className='ordered-items-list'>
-                {orderedItems}
-              </ul>
             </div>
-          )
-        }
+            <ul className="ordered-items-list">{orderedItems}</ul>
+          </div>
+        );
       }
     }
+  }
 }
-
 
 export default CheckShow;
--- frontend/components/items/items_index.jsx
+++ frontend/components/items/items_index.jsx
@@ -1,10 +1,9 @@
-import React from 'react';
-import { Route, Link } from 'react-router-dom';
-import { ItemIndexItem } from './items_index_item';
-
+import React from "react";
+import { Route, Link } from "react-router-dom";
+import { ItemIndexItem } from "./items_index_item";
 
 class ItemIndex extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.fetchItems = this.props.fetchItems;
     this.addItemOnCheck = this.props.addItemOnCheck;
@@ -12,26 +11,32 @@ class ItemIndex extends React.Component {
     this.handleItemSelection = this.handleItemSelection.bind(this);
   }
 
-  handleItemSelection(checkId, itemId){
-    this.addItemOnCheck(checkId, itemId).then(() => this.fetchCheck(this.props.checkId))
+  handleItemSelection(checkId, itemId) {
+    this.addItemOnCheck(checkId, itemId).then(() =>
+      this.fetchCheck(this.props.checkId)
+    );
   }
 
-  render(){
+  render() {
     let items;
     if (this.props.items.toString() === "[object Object]") {
-      return null
+      return null;
     } else {
-      items = this.props.items.map ((item, i) => {
-        return <ItemIndexItem key={i} handleItemSelection={this.handleItemSelection} checkId={this.props.checkId} itemId={item.id} name={item.name} price={item.price} />
-      })
+      items = this.props.items.map((item, i) => {
+        return (
+          <ItemIndexItem
+            key={i}
+            handleItemSelection={this.handleItemSelection}
+            checkId={this.props.checkId}
+            itemId={item.id}
+            name={item.name}
+            price={item.price}
+          />
+        );
+      });
     }
-    return (
-        <ul className='modal-list item-modal-list'>
-          {items}
-        </ul>
-    )
+    return <ul className="modal-list item-modal-list">{items}</ul>;
   }
 }
-
 
 export default ItemIndex;
