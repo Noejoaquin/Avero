8765b9de6375199dabe03474579628b099afca1f
Noe <noe.joaquin.james@gmail.com>
2018-02-02 23:18:09 -0500

    fix table error action

--- frontend/actions/table_actions.js
+++ frontend/actions/table_actions.js
@@ -1,21 +1,15 @@
-import * as TableApiUtil from '../util/table_util';
+import * as TableApiUtil from "../util/table_util";
 
 export const RECEIVE_TABLES = "RECEIVE_TABLES";
 export const RECEIVE_CHECK_ERRORS = "RECEIVE_CHECK_ERRORS";
 
-export const receiveErrors = (errors) => ({
-  type: RECEIVE_TABLE_ERRORS,
-  errors
-})
-
-export const receiveTables = (tables) => ({
+export const receiveTables = tables => ({
   type: RECEIVE_TABLES,
   tables
-})
+});
 
 export const fetchTables = () => dispatch => {
-  return TableApiUtil.fetchTables()
-                     .then((tables) => dispatch(receiveTables(tables)),
-                      (errors) => dispatch(receiveErrors(errors))
-                    );
+  return TableApiUtil.fetchTables().then(tables =>
+    dispatch(receiveTables(tables))
+  );
 };
